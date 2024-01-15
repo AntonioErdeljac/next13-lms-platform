ce46096fe80d7d16ba705abaf71efcffa24eebfb
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-01-15 13:58:31 +0000

    Add new categories to database seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,19 +4,24 @@ const database = new PrismaClient();
 
 async function main() {
   try {
-    await database.category.createMany({
+    const r = await database.category.createMany({
       data: [
-        { name: "Computer Science" },
-        { name: "Music" },
-        { name: "Fitness" },
-        { name: "Photography" },
-        { name: "Accounting" },
-        { name: "Engineering" },
-        { name: "Filming" },
-      ]
+        { 
+          id: "76696e5a-c6f9-430e-a702-fa3f4f24fe7e",
+          name: "Python" 
+        },
+        { 
+          id: "838b5a2d-a2e3-4e9b-a5c9-3d992c59a775",
+          name: "NodeJS" 
+        },
+        { 
+          id: "15a44bad-d1a9-442b-a81f-87a51fa3c924",
+          name: "Django" 
+        },
+      ],
+      skipDuplicates: true,
     });
-
-    console.log("Success");
+    console.log(`Success: ${r.count} categories created.`);
   } catch (error) {
     console.log("Error seeding the database categories", error);
   } finally {
