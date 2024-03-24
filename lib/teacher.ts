adecdb1f9b05beb93c380b619ff3471554d4c9ec
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2024-03-24 12:24:17 +0530

    Update teacher.ts

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,12 +1,3 @@
 export const isTeacher = (userId?: string | null) => {
-  const teacherIdsString = process.env.NEXT_PUBLIC_TEACHER_IDS || '';
-  const teacherIdsArray = teacherIdsString.split(',');
-
-  // Check if userId is defined and is a string before using it
-  if (typeof userId === 'string') {
-    return teacherIdsArray.includes(userId);
-  } else {
-    // Handle the case where userId is null or undefined
-    return false; // Or you can choose an alternative behavior
-  }
+  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
 }
