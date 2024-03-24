ed58755da58ced5d9d27f06b77321be44710f1a1
Dev Ambience <125862598+Devambience@users.noreply.github.com>
2024-03-24 16:40:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   images: {
     domains: [
       "utfs.io"
     ]
-  }
-}
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
